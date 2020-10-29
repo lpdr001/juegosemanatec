0f6c9b243aa39a817036a1405fd4c2ce8cde75f0
Alex888666 <alexhdlt@hotmail.com>
2020-10-28 21:39:32 -0600

    Add files via upload

---
+++ AlexPaoPaint.py
@@ -0,0 +1,91 @@
+""" Alejandro Hernández De la Torre / Lidia Paola Díaz Ramirez """
+from turtle import *
+from freegames import vector
+import math
+
+def line(start, end):
+    "Draw line from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    goto(end.x, end.y)
+
+def square(start, end):
+    "Draw square from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    for count in range(4):
+        forward(end.x - start.x)
+        left(90)
+    end_fill()
+
+def circle(start, end):
+    "Draw circle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    for count in range(360):
+        forward(math.sin(math.radians(1))*(math.sqrt((end.x - start.x)**2+(end.y - start.y)**2)))
+        left(1)
+    end_fill()
+
+def rectangle(start, end):
+    "Draw rectangle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    for count in range(2):
+        forward (end.x - start.x)
+        left (90)
+        forward(end.y - start.y)
+        left(90)
+    end_fill()
+    
+def triangle(start, end):
+    "Draw triangle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+    end_fill()
+    
+def tap(x, y):
+    "Store starting point or draw shape."
+    start = state['start']
+    if start is None:
+        state['start'] = vector(x, y)
+    else:
+        shape = state['shape']
+        end = vector(x, y)
+        shape(start, end)
+        state['start'] = None
+
+def store(key, value):
+    "Store value in state at key."
+    state[key] = value
+
+state = {'start': None, 'shape': line}
+setup(420, 420, 370, 0)
+onscreenclick(tap)
+listen()
+onkey(undo, 'u')
+onkey(lambda: color('black'), 'N')
+onkey(lambda: color('yellow'), 'Y')
+onkey(lambda: color('green'), 'G')
+onkey(lambda: color('Red'), 'R')
+onkey(lambda: color('blue'), 'B')
+onkey(lambda: color('red'), 'R')
+onkey(lambda: color('pink'), 'P')
+onkey(lambda: store('shape', line), 'l')
+onkey(lambda: store('shape', square), 's')
+onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', rectangle), 'r')
+onkey(lambda: store('shape', triangle), 't')
+done()
